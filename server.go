ab8dbd41ad72a6eaff228ea836e432bad94d7040
Kenny Grant <kennygrant@gmail.com>
2016-12-20 11:04:52 +0000

    Added autocert support for let's encrypt certs using StartTLSAutocert, adjusted default server config

--- server.go
+++ server.go
@@ -2,14 +2,16 @@
 package server
 
 import (
+	"crypto/tls"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
-)
 
-// TODO: Graceful restart would be nice - http://grisha.org/blog/2014/06/03/graceful-restart-in-golang/
+	"golang.org/x/crypto/acme/autocert"
+)
 
 // Logger interface for a simple logger (the stdlib log pkg and the fragmenta log pkg conform)
 type Logger interface {
@@ -49,7 +51,7 @@ func New() (*Server, error) {
 		configProduction:  make(map[string]string),
 		configDevelopment: make(map[string]string),
 		configTest:        make(map[string]string),
-		Logger:            log.New(os.Stderr, "fragmenta: ", log.LstdFlags), // default to a stderr logger
+		Logger:            log.New(os.Stderr, "fragmenta: ", log.LstdFlags),
 	}
 
 	err := s.readConfig()
@@ -97,16 +99,102 @@ func (s *Server) Timef(format string, start time.Time, v ...interface{}) {
 	s.Logf(format, args...)
 }
 
+// PortString returns a string port suitable for passing to http.Server
+func (s *Server) PortString() string {
+	return fmt.Sprintf(":%d", s.port)
+}
+
 // Start starts an http server on the given port
 func (s *Server) Start() error {
-	p := fmt.Sprintf(":%d", s.port)
-	return http.ListenAndServe(p, nil)
+	server := &http.Server{
+		Addr:         s.PortString(),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	return server.ListenAndServe()
 }
 
 // StartTLS starts an https server on the given port
+// with tls cert/key from config keys.
 func (s *Server) StartTLS() error {
-	port := fmt.Sprintf(":%d", s.port)
 	cert := s.Config("tls_cert")
 	key := s.Config("tls_key")
-	return http.ListenAndServeTLS(port, cert, key, nil)
+	return s.ConfiguredTLSServer(nil).ListenAndServeTLS(cert, key)
+}
+
+// StartTLSAutocert starts an https server on the given port
+// by requesting certs from an ACME provider.
+// The server must be on a public IP which matches the
+// DNS for the domains.
+func (s *Server) StartTLSAutocert(email string, domains string) error {
+	autocertDomains := strings.Split(domains, " ")
+	certManager := &autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		Email:      email,                                      // Email for problems with certs
+		HostPolicy: autocert.HostWhitelist(autocertDomains...), // Domains to request certs for
+		Cache:      autocert.DirCache("secrets"),               // Cache certs in secrets folder
+	}
+	server := s.ConfiguredTLSServer(certManager)
+	return server.ListenAndServeTLS("", "")
+}
+
+// ConfiguredTLSServer returns a TLS server instance with a secure config
+// this server has read/write timeouts set to 20 seconds,
+// prefers server cipher suites and only uses certain accelerated curves
+// see - https://blog.gopheracademy.com/advent-2016/exposing-go-on-the-internet/
+func (s *Server) ConfiguredTLSServer(certManager *autocert.Manager) *http.Server {
+
+	return &http.Server{
+		// Set the port in the preferred string format
+		Addr: s.PortString(),
+		/*
+			// The default server from net/http has no timeouts
+			ReadTimeout:  20 * time.Second,
+			WriteTimeout: 20 * time.Second,
+			IdleTimeout:  120 * time.Second,
+		*/
+		// This TLS config follows recommendations in the above article
+		TLSConfig: &tls.Config{
+			// Pass in a cert manager if you want one set
+			// this will only be used if the server Certificates are empty
+			GetCertificate: certManager.GetCertificate,
+
+			// VersionTLS11 or VersionTLS12 would exclude many browsers
+			// inc. Android 4.x, IE 10, Opera 12.17, Safari 6
+			// So unfortunately not acceptable as a default yet
+			// Current default here for clarity
+			MinVersion: tls.VersionTLS10,
+
+			// Causes servers to use Go's default ciphersuite preferences,
+			// which are tuned to avoid attacks. Does nothing on clients.
+			PreferServerCipherSuites: true,
+			// Only use curves which have assembly implementations
+			CurvePreferences: []tls.CurveID{
+				tls.CurveP256,
+				tls.X25519, // Go 1.8 only
+			},
+		},
+	}
+
+}
+
+// StartRedirectAll starts redirecting from port given to the given url
+// this should be called before StartTLS if redirecting to https
+func (s *Server) StartRedirectAll(p int, url string) {
+	port := fmt.Sprintf(":%d", p)
+	// Listen and server on port p in a separate goroutine
+	go func() {
+		http.ListenAndServe(port, &redirectHandler{redirect: url})
+	}()
+}
+
+// redirectHandler is useful if serving tls direct (not behind a proxy)
+// and a redirect from port 80 is required.
+type redirectHandler struct {
+	redirect string
+}
+
+// ServeHTTP on this handler simply redirects to the main site
+func (m *redirectHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, m.redirect, http.StatusMovedPermanently)
 }
